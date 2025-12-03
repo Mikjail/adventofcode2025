86808d37b82cf9e820f2019f529788bcff4ca7c1
Mikjail Salazar <mikjailsalazar@gmail.com>
2025-12-03 22:12:40 +0000

    puzzle3: at the moment just a cuadratic for works

--- day03/solve.go
+++ day03/solve.go
@@ -1,8 +1,36 @@
 package day03
 
+import (
+	"spissable/advent-of-go-template/utils"
+	"strconv"
+)
+
 func SolvePuzzle1(input string) int {
-	// TODO: solve puzzle 1
-	return 0
+	lines := utils.GetLineList(input)
+	result := 0
+
+	for _, line := range lines {
+		count := len(line)
+		max_left := 0
+
+		for i := 0; i < count-1; i++ {
+			numberLeft := string(line[i])
+			for j := i + 1; j < count; j++ {
+				numberRight := string(line[j])
+				number, err := strconv.Atoi(numberLeft + numberRight)
+				if err == nil {
+					if max_left < number {
+						max_left = number
+					}
+				}
+			}
+		}
+
+		result += max_left
+
+	}
+
+	return result
 }
 
 func SolvePuzzle2(input string) int {
--- day03/solve_test.go
+++ day03/solve_test.go
@@ -4,12 +4,15 @@ import (
 	"spissable/advent-of-go-template/day03"
 	"spissable/advent-of-go-template/utils"
 	"testing"
+
+	"github.com/alecthomas/assert/v2"
 )
 
 func TestSolvePuzzle1(t *testing.T) {
 	input := utils.ReadInput(t)
 	result := day03.SolvePuzzle1(input)
 	utils.LogResult(t, result)
+	assert.Equal(t, result, 17034)
 }
 
 func TestSolvePuzzle2(t *testing.T) {
