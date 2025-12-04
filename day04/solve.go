45be12dff2ad651bad16482ef67d6d46281c12e9
Mikjail Salazar <mikjailsalazar@gmail.com>
2025-12-04 11:50:56 +0000

    day4: start doing iterations through adjacent cols

--- day04/solve.go
+++ day04/solve.go
@@ -1,11 +1,83 @@
 package day04
 
+import "spissable/advent-of-go-template/utils"
+
 func SolvePuzzle1(input string) int {
-	// TODO: solve puzzle 1
-	return 0
+	lines := utils.GetLineList(input)
+	paper_roll := "@"
+	arr := utils.ParseArrayStringIntoMatrix(lines)
+	rows := len(arr)
+	cols := len(arr[0])
+
+	accessibleRolls := 0
+
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			if arr[row][col] == paper_roll {
+				neighborCount := checkAdjacentPapers(arr, row, col, rows, cols, paper_roll)
+				if neighborCount < 4 {
+					accessibleRolls++
+				}
+			}
+		}
+	}
+
+	return accessibleRolls
+}
+
+func checkAdjacentPapers(arr [][]string, row, col, rows, cols int, target string) int {
+	directions := [][]int{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+
+	count := 0
+	for _, dir := range directions {
+		newRow := row + dir[0]
+		newCol := col + dir[1]
+
+		if newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols {
+			if arr[newRow][newCol] == target {
+				count++
+			}
+		}
+	}
+
+	return count
 }
 
 func SolvePuzzle2(input string) int {
-	// TODO: solve puzzle 2
-	return 0
+	lines := utils.GetLineList(input)
+	paper_roll := "@"
+	arr := utils.ParseArrayStringIntoMatrix(lines)
+	ROWS := len(arr)
+	COLS := len(arr[0])
+	totalRemoved := 0
+
+	for {
+		toRemove := [][]int{}
+
+		for row := 0; row < ROWS; row++ {
+			for col := 0; col < COLS; col++ {
+				if arr[row][col] == paper_roll {
+					neighborCount := checkAdjacentPapers(arr, row, col, ROWS, COLS, paper_roll)
+					if neighborCount < 4 {
+						toRemove = append(toRemove, []int{row, col})
+					}
+				}
+			}
+		}
+
+		if len(toRemove) == 0 {
+			break
+		}
+
+		for _, pos := range toRemove {
+			arr[pos[0]][pos[1]] = "."
+			totalRemoved++
+		}
+	}
+
+	return totalRemoved
 }
--- day04/solve_test.go
+++ day04/solve_test.go
@@ -4,16 +4,20 @@ import (
 	"spissable/advent-of-go-template/day04"
 	"spissable/advent-of-go-template/utils"
 	"testing"
+
+	"github.com/alecthomas/assert/v2"
 )
 
 func TestSolvePuzzle1(t *testing.T) {
 	input := utils.ReadInput(t)
 	result := day04.SolvePuzzle1(input)
 	utils.LogResult(t, result)
+	assert.Equal(t, result, 1395)
 }
 
 func TestSolvePuzzle2(t *testing.T) {
 	input := utils.ReadInput(t)
 	result := day04.SolvePuzzle2(input)
 	utils.LogResult(t, result)
+	assert.Equal(t, result, 8451)
 }
